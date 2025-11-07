be92fe915d9d58b3eb3d95aa374bd3066682af87
Martin Trajkovski <martintrajkovski70@gmail.com>
2025-11-07 20:41:07 +0100

    - Initial commit

---
+++ src/main/java/com/example/SpringAIDocsParseReadApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class SpringAIDocsParseReadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAIDocsParseReadApplication.class, args);
+  }
+}
---
+++ src/main/java/com/example/rest/MainController.java
@@ -0,0 +1,14 @@
+package com.example.rest;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+
+
+}
